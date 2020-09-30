bd34b654d7b4ea054c15d003641aa0c1cb71a7ed
Vladimir6332 <vladimir6332@gmail.com>
2020-10-01 00:28:49 +0300

    feat: complete vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,83 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this._typeOfMachine = isDirect ? 'direct' : 'reverse';
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  }
+
+  encrypt(str, key) {
+    const strArr = str.split('');
+    const keyArr = key.split('');
+    let counter = 0;
+
+    const result = strArr.reduce((acc, val, index) => {
+      if (!/[A-Z]/.test(val.toUpperCase())) {
+        counter++;
+        acc.push(val);
+        return acc;
+      }
+
+      const indexCharFromKey = (index - counter) % keyArr.length;
+      const codeCharFromKey = keyArr[indexCharFromKey]
+        .toUpperCase()
+        .charCodeAt(0);
+      const codeCurrentChar = val.toUpperCase().charCodeAt(0);
+
+      const deltaPositionFromStartAlphabet =
+        codeCurrentChar - 'A'.charCodeAt(0);
+      const deltaPositionFromStartAlphabetKey =
+        codeCharFromKey - 'A'.charCodeAt(0);
+      const newChar = this.alphabet[
+        (deltaPositionFromStartAlphabetKey + deltaPositionFromStartAlphabet) %
+          this.alphabet.length
+      ];
+      console.log(val.toUpperCase().charCodeAt(0));
+
+      acc.push(newChar);
+      return acc;
+    }, []);
+
+    if (this._typeOfMachine === 'direct') return result.join('');
+    else if (this._typeOfMachine === 'reverse')
+      return result.reverse().join('');
+  }
+
+  decrypt(str, key) {
+    const strArr = str.split('');
+    const keyArr = key.split('');
+    let counter = 0;
+
+    const result = strArr.reduce((acc, val, index) => {
+      if (!/[A-Z]/.test(val.toUpperCase())) {
+        counter++;
+        acc.push(val);
+        return acc;
+      }
+
+      const indexCharFromKey = (index - counter) % keyArr.length;
+      const codeCharFromKey = keyArr[indexCharFromKey]
+        .toUpperCase()
+        .charCodeAt(0);
+      const codeCurrentChar = val.toUpperCase().charCodeAt(0);
+
+      const deltaPositionFromStartAlphabet =
+        codeCurrentChar - 'A'.charCodeAt(0);
+      const deltaPositionFromStartAlphabetKey =
+        codeCharFromKey - 'A'.charCodeAt(0);
+      const newChar = this.alphabet[
+        (deltaPositionFromStartAlphabet -
+          deltaPositionFromStartAlphabetKey +
+          this.alphabet.length) %
+          this.alphabet.length
+      ];
+      acc.push(newChar);
+      return acc;
+    }, []);
+
+    if (this._typeOfMachine === 'direct') return result.join('');
+    else if (this._typeOfMachine === 'reverse')
+      return result.reverse().join('');
   }
 }
 
